c77162d6fdaa8134f8db139f109c921fa6402b7c
VikramG <VikramG@boxer.co.za>
2023-09-03 20:52:05 +0200

    4-lombok-logging

--- src/main/java/guru/springframework/spring6restmvc/service/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/service/BeerServiceImpl.java
@@ -2,16 +2,20 @@ package guru.springframework.spring6restmvc.service;
 
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+        log.debug("Get Beer by Id - in service. Id: "+id.toString());
+
         return Beer.builder()
                 .id(id)
                 .version(1)
