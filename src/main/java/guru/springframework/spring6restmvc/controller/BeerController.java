ec75638f11ac0c55cb849bf32ac8891dee9bed6e
VikramG <VikramG@boxer.co.za>
2023-09-03 22:32:47 +0200

    5-list-beer

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -5,14 +5,23 @@ import guru.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+       return beerService.listBeers();
+    }
+
 
     public Beer getBeerById(UUID id){
         log.debug("Get Beer by Id - in controller");
--- src/main/java/guru/springframework/spring6restmvc/service/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/service/BeerService.java
@@ -2,8 +2,11 @@ package guru.springframework.spring6restmvc.service;
 
 import guru.springframework.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
