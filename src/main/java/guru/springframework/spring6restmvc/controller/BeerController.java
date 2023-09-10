01472e400b30e50a1a456cecde4f50ef574fcbea
VikramG <VikramG@boxer.co.za>
2023-09-10 18:47:14 +0200

    10-response-header

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -4,10 +4,12 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.ServerRequest;
 
 import java.util.List;
 import java.util.UUID;
@@ -22,8 +24,12 @@ public class BeerController {
 
     @PostMapping
     public ResponseEntity handlePost(@RequestBody Beer beer){
+        HttpHeaders headers = new HttpHeaders();
+
         Beer savedBeer = beerService.saveNewBeer(beer);
-        return new ResponseEntity(HttpStatus.CREATED);
+        headers.add("Location","/api/v1/beer/"+savedBeer.getId().toString());
+
+        return new ResponseEntity(headers,HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
